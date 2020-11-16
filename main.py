45323bd9f5cc6aad484346bb5d0bea3f3d7981be
EmperorPa가 <woghkd0719@gmail.com>
2020-11-16 20:25:26 +0900

    메인함수 일부 수정

--- main.py
+++ main.py
@@ -13,16 +13,24 @@ def main():
     if choice == 0:
         sys.exit(0)
     elif choice == 1: # 상품 등록 확인/상품 판매 확인 선택
-        sel = int(input('등록 확인시에는 1입력, 판매 확인시에는 2입력 >> '))
+        print('1: 상품 등록 확인')
+        print('2: 상품 판매 확인')
+        sel = int(input('실행하고자 하는 번호를 입력해주세요 >> '))
         if sel == 1: # 등록 확인
-            print('등록확인 모드')
+            print('등록확인 모드(쿠팡(www.coupang.com), 신세계(www.ssg.com))')
+            print('ex) 아이폰 12 미니 자급제 64GB')
             searchWord = str(input('상품 검색(띄어쓰기 구분이 명확할수록 검색 정확도가 높음) >> '))
             keywords = searchWord.split()
             data = objCarwlShoppingMall.foreignLangSearch(keywords)
             print(data)
             print ('구현중')
         elif sel == 2:
-            print('판매확인 모드')
+            print('판매확인 모드(쿠팡(www.coupang.com), 신세계(www.ssg.com))')
+            checkurl = str(input('상품 url입력 >> '))
+            if 'coupang.com' not in checkurl and 'ssg.com' not in checkurl:
+                print ('미지원 쇼핑몰입니다. 다시 수행해주세요')
+                sys.exit(0)
+            
             print ('구현중')
         else:
             print ('오입력이 발생 하였습니다. 다시 수행해주세요')
